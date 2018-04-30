99af589b4b46fd29ce173a59a620271c3bfc1599
Joanne Kim <joannehyelee@gmail.com>
2018-04-29 20:25:59 -0700

    Return tweets

--- liri.js
+++ liri.js
@@ -6,12 +6,30 @@ var request = require("request");
 var command = process.argv[2];
 
 var client = new Twitter(keys.twitter);
-console.log(client);
+// console.log(client);
 
 var params = {
     screen_name: 'joannehyelee',
-    count: 1
+    count: 20
 }
+
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+
+    var myTweets = tweets;
+    // tweets returns as an array
+
+    if (!error) {
+
+        for (var i = 0; i < myTweets.length; i++) {
+            console.log("--------------------------------------");
+            console.log(myTweets[i].text);
+        }
+
+    } else {
+        console.log(error);
+    }
+ });
+
 
 // var tweetsUrl = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=joannehyelee&count=2";
 
@@ -24,10 +42,6 @@ var params = {
 //         console.log(error);
 //     }
 // });
-
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    console.log(tweets);
- });
 
 // if (command === "my-tweets") {
 //     keys.client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
